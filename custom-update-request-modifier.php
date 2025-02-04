62379ea4ace4ef9efb8b92025371437a1c1ad207
Robert DeVore <deviodigital@gmail.com>
2025-02-04 03:02:04 -0500

    :package: NEW: Added WPCom Check to restrict plugin usage on wordpress.com

--- custom-update-request-modifier.php
+++ custom-update-request-modifier.php
@@ -39,6 +39,15 @@ $myUpdateChecker = PucFactory::buildUpdateChecker(
 // Set the branch that contains the stable release.
 $myUpdateChecker->setBranch( 'main' );
 
+// Check if Composer's autoloader is already registered globally.
+if ( ! class_exists( 'RobertDevore\WPComCheck\WPComPluginHandler' ) ) {
+    require_once __DIR__ . '/vendor/autoload.php';
+}
+
+use RobertDevore\WPComCheck\WPComPluginHandler;
+
+new WPComPluginHandler( plugin_basename( __FILE__ ), 'https://robertdevore.com/why-this-plugin-doesnt-support-wordpress-com-hosting/' );
+
 /**
  * Current plugin version.
  */
